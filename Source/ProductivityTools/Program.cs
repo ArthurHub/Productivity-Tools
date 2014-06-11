31a159798e5305e123103713770e8b8c480848bb
Arthur <thekingarthur@gmail.com>
2014-06-11 16:04:00 +0300

    embed all references into the exe so it will be single file

---
+++ Source/ProductivityTools/Program.cs
@@ -0,0 +1,62 @@
+﻿// "Therefore those skilled at the unorthodox
+// are infinite as heaven and earth,
+// inexhaustible as the great rivers.
+// When they come to an end,
+// they begin again,
+// like the days and months;
+// they die and are reborn,
+// like the four seasons."
+//
+// - Sun Tsu,
+// "The Art of War"
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+namespace ProductivityTools
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+            App.Main();
+        }
+
+        /// <summary>
+        /// Using references dlls embedded in the assembly so the tool can be a single file.
+        /// </summary>
+        /// <remarks>
+        /// <![CDATA[
+        /// <EmbeddedResource Include="@(ReferenceCopyLocalPaths)" Condition="'%(ReferenceCopyLocalPaths.Extension)' == '.dll'">
+        /// <LogicalName>%(ReferenceCopyLocalPaths.Filename)%(ReferenceCopyLocalPaths.Extension)</LogicalName>
+        /// </EmbeddedResource>
+        /// ]]>
+        /// </remarks>
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var assemblyName = new AssemblyName(args.Name);
+
+            string path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+            {
+                path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+            }
+
+            using (var stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream != null)
+                {
+                    var assemblyRawBytes = new byte[stream.Length];
+                    stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                    return Assembly.Load(assemblyRawBytes);
+                }
+                return null;
+            }
+        }
+    }
+}
